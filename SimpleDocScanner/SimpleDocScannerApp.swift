00826e87203b122bbb99a36dea8e28ec4733958f
James Babiak <46328753+jamesbabiak@users.noreply.github.com>
2025-07-23 17:41:51 -0400

    Initial Commit

---
+++ SimpleDocScanner/SimpleDocScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleDocScannerApp.swift
+//  SimpleDocScanner
+//
+//  Created by James Babiak on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleDocScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
